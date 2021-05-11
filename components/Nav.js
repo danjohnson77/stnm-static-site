c87472c007f9174c126d42b045fd04c43ea02452
Daniel Johnson <danjohnson77@gmail.com>
2021-05-10 17:44:32 -0700

    fixed mobile icon

--- components/Nav.js
+++ components/Nav.js
@@ -20,45 +20,22 @@ const Nav = () => {
   };
 
   return (
-    <nav className="fixed min-w-full z-50">
+    <nav
+      className="fixed min-w-full z-50"
+      onClick={() => open && setOpen(!open)}
+    >
       <div
-        className="w-8 cursor-pointer bg-black bg-opacity-50 rounded-md lg:hidden fixed m-5"
+        className="w-8 cursor-pointer bg-black bg-opacity-50 rounded-md lg:hidden fixed m-5 text-center"
         onClick={() => setOpen(!open)}
       >
-        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-          <line
-            x1="10"
-            y1="15"
-            x2="90"
-            y2={`${open ? "85" : "15"}`}
-            stroke="white"
-            strokeWidth="4px"
-          />
-          <line
-            x1="10"
-            y1="50"
-            x2="90"
-            y2="50
-        "
-            stroke="white"
-            strokeWidth="4px"
-            style={{ display: open && "none" }}
-          />
-          <line
-            x1="10"
-            y2={`${open ? "15" : "85"}`}
-            x2="90"
-            y1="85
-        "
-            stroke="white"
-            strokeWidth="4px"
-          />
-        </svg>
+        <i className={`fas ${open ? "fa-times" : "fa-bars"} text-white `}></i>
       </div>
 
       <div
-        className={`text-white transition-colors duration-500 text-2xl text-center font-serif justify-center bg-transparent bg-opacity-75 ${
-          open ? "flex flex-col h-screen bg-black bg-opacity-100" : "hidden"
+        className={`text-white text-2xl text-center font-serif justify-center bg-transparent bg-opacity-75  ${
+          open
+            ? "flex flex-col h-screen bg-black bg-opacity-100 opacity-100"
+            : "hidden"
         } lg:flex lg:justify-between lg:p-5 ${shadeNav && "bg-black"}`}
       >
         <h1 className={`${open && "hidden"} 'cursor-pointer'`}>
